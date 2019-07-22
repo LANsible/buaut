6e420cf042edd9651e721a006c9c370baa196eef
wilmardo <info@wilmardenouden.nl>
2019-07-22 16:03:24 +0200

    refactor: Move creation of requests to helpers, monetary account id to main

--- buaut/helpers.py
+++ buaut/helpers.py
@@ -1,5 +1,12 @@
+# Typehinting
+from typing import List, Tuple
+
+import re
+import validators
+
 from bunq.sdk.client import Pagination
 from bunq.sdk.model.generated import endpoint
+from bunq.sdk.model.generated.object_ import Pointer, Amount
 
 
 def get_monetary_account_id(type: str, value: str) -> int:
@@ -11,6 +18,9 @@ def get_monetary_account_id(type: str, value: str) -> int:
 
   Returns:
       int: monetary account id
+
+  Raises:
+      ValueError: If match is not found
   """
   pagination = Pagination()
   pagination.count = 25
@@ -22,6 +32,8 @@ def get_monetary_account_id(type: str, value: str) -> int:
       if alias.type_ == type and alias.value == value:
         return monetaryaccount.MonetaryAccountBank.id_
 
+  raise ValueError
+
 
 def convert_to_valid_amount(amount: any) -> str:
     """Convert any datatype to a valid amount (xx.xx)
@@ -32,5 +44,59 @@ def convert_to_valid_amount(amount: any) -> str:
     Returns:
         str: Amount in valid currency string
     """
-    # Format trick from: https://stackoverflow.com/a/6539677
+    # Source: https://stackoverflow.com/a/6539677
     return "{0:.2f}".format(amount)
+
+
+def comma_seperated_string_to_list(string: str) -> List[str]:
+  """Convert comma seperated string to list
+
+  Args:
+      string (str): Comma seperated string to split
+
+  Returns:
+      List: List contain the items of the string
+  """
+  # Source: https://stackoverflow.com/a/12760144
+  pattern = re.compile("^\s+|\s*,\s*|\s+$")
+  return pattern.split(string)
+
+
+def create_request_batch(monetary_account_id: int, requests: List[Tuple[str, float]], description: str, currency: str):
+  """Create request batch from a list of requests
+
+  Args:
+      monetary_account_id (int): Account id where the requests are made from
+      requests (List[tuple]): List of tuples containing email and amount
+      description (str): [description]
+      currency (str): [description]
+  """
+  request_inqueries: List[dict] = []
+  total_amount_inquired: int = 0
+
+   for email, amount in requests:
+      # Check if valid email
+      # TODO: Create some logging class and exit with message
+      if not validators.email(email):
+        exit(1)
+
+      # Add amount to total
+      total_amount_inquired += amount
+      # Convert to valid Bunq currency string
+      amount: str = convert_to_valid_amount(amount)
+      # Create request and append to request_inqueries list
+      request_inqueries.append({
+          'amount_inquired': Amount(amount, currency),
+          'counterparty_alias': Pointer(type_='EMAIL', value=email),
+          'description': description,
+          'allow_bunqme': True
+      })
+
+    # Convert to valid Bunq currency string
+    total_amount_inquired: str = convert_to_valid_amount(
+        total_amount_inquired)
+    endpoint.RequestInquiryBatch.create(
+        request_inquiries=request_inqueries,
+        total_amount_inquired=Amount(total_amount_inquired, currency),
+        monetary_account_id=monetary_account_id
+    )
