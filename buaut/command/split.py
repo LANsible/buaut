63b87d52ecb8ca200e3f8f22c2ee490e7a91fb38
wilmardo <info@wilmardenouden.nl>
2019-09-01 20:17:05 +0200

    wip(split): Almost working, running into a little bug
    Signed-off-by: wilmaro <w.denouden@intermax.nl>

--- buaut/command/split.py
+++ buaut/command/split.py
@@ -6,13 +6,10 @@ import validators
 from bunq.sdk.client import Pagination
 from bunq.sdk.exception import BunqException
 from bunq.sdk.model.generated import endpoint
-from bunq.sdk.model.generated.object_ import Pointer, Amount, Payment
+from bunq.sdk.model.generated.object_ import Pointer, Amount
+from pprint import pprint
 
 from buaut import helpers
-
-# Global variables
-INCLUDES: List[str] = []
-EXCLUDES: List[str] = []
 
 
 @click.command()
@@ -35,37 +32,72 @@ EXCLUDES: List[str] = []
     type=click.STRING
 )
 def split(ctx, get: List[Tuple[str, float]], includes: str, excludes: str):
-  """Split payments to certain users
+    """Split payments to certain users
 
-  Args:
-      ctx ([type]): Click object containing the arguments from global
-      get ([tuple]): List of users to request from
-      includes (str): Comma seperated string containing includes
-      excludes (str): Comma seperated string containing excludes
-  """
-  excludes: List[str] = helpers.comma_seperated_string_to_list(excludes)
-  includes: List[str] = helpers.comma_seperated_string_to_list(includes)
-  monetary_account_id: int = ctx.obj.get('monetary_account_id')
+    Args:
+        ctx ([type]): Click object containing the arguments from global
+        get ([tuple]): List of users to request from
+        includes (str): Comma seperated string containing includes
+        excludes (str): Comma seperated string containing excludes
+    """
+    monetary_account_id: int = ctx.obj.get('monetary_account_id')
+    currency: str = ctx.obj.get('currency')
 
-  unsplit_payments = get_all_unsplit_payments(monetary_account_id)
-  # Split the bill!
-  if float(payment.amount.value) > 0:
-    continue  # not a afschrijving
-  # Convert to positive
-  amount_to_split = float(payment.amount.value) * -1
-  description = {
-      'id': payment.id_,
-      'from': payment.counterparty_alias.label_monetary_account.display_name,
-      'description': payment.description,
-      'created': payment.created
-  }
-  request_inqueries = []
-  for iban, percentage in SPLIT.items():
-    # Calculate amount and format (https://stackoverflow.com/a/6539677)
-    amount = "{0:.2f}".format(amount_to_split * (percentage / 100))
+    excludes_list: List[str] = []
+    includes_list: List[str] = []
+
+    if includes:
+        includes_list = helpers.comma_seperated_string_to_list(includes)
+    if excludes:
+        excludes_list = helpers.comma_seperated_string_to_list(excludes)
+
+    # Get all unsplit events
+    # NOTE: Using events since we need to pass event_id to the requestInquiry
+    # to get a correct reference
+    unsplit_events: List[endpoint.Event] = get_all_unsplit_events(
+        monetary_account_id)
+
+    # Filter out all non relevant events
+    included_events: List[endpoint.Event] = filter_excluded_events(
+        events=unsplit_events, includes=includes_list, excludes=excludes_list)
+
+    if included_events:
+        # Create new list of tuples to fill with amounts
+        requests: List[Tuple[str, float]] = []
+        for event in included_events:
+            # Split the bill!
+            payment = event.object_.Payment
+
+            # Convert to positive
+            amount_to_split: float = float(payment.amount.value) * -1
+            description: dict = {
+                'id': payment.id_,
+                'from': payment.counterparty_alias.label_monetary_account.display_name,
+                'description': payment.description,
+                'created': payment.created
+            }
+
+            for email, percentage in get:
+                normalized_percentage: float = float(percentage) / 100
+                # Calculate amount to request
+                amount: float = amount_to_split * normalized_percentage
+                requests.append((email, amount))
+
+            # Create request batch for payment
+            helpers.create_request_batch(
+                monetary_account_id=monetary_account_id,
+                requests=requests,
+                description=str(description),
+                currency=currency,
+                event_id_field_for_request=event._id
+            )
+            # Request sent so empty requests
+            requests = []
+    else:
+        exit(0)
 
 
-def get_all_unsplit_payments(monetary_account_id: int) -> List[Payment]:
+def get_all_unsplit_events(monetary_account_id: int) -> List[endpoint.Event]:
     """Get all unsplit payments for a certain account
 
     Args:
@@ -75,60 +107,86 @@ def get_all_unsplit_payments(monetary_account_id: int) -> List[Payment]:
         List[Payment]: List of unsplit bunq payments
     """
 
-    unsplit_payments: List[Payment] = []
+    events: List[endpoint.Event] = []
+    unsplit_events: List[endpoint.Event] = []
 
-    # Loop until we break the loop
+    # Loop until we return
     while True:
-      # Check if first iteration (unsplit_payments empty)
-      if unsplit_payments:
-          # We will loop over the payments in baches of 50
-          pagination = Pagination()
-          pagination.count = 50
-          params = pagination.url_params_count_only
+        # Check if first iteration (unsplit_payments empty)
+        if not events:
+            # We will loop over the payments in baches of 50
+            pagination = Pagination()
+            pagination.count = 50
+            params = pagination.url_params_count_only
+        else:
+            # When there is already a paged request, you can get the next page from it, no need to create it ourselfs:
+            try:
+                params = events.pagination.url_params_previous_page
+            except BunqException:
+                break
 
-      else:
-          # When there is already a paged request, you can get the next page from it, no need to create it ourselfs:
-          try:
-              params = events.pagination.url_params_previous_page
-          except BunqException:
-              break
+        # Add parameters to only list for current monetary_account_id
+        params['monetary_account_id'] = monetary_account_id
+        params['display_user_event'] = 'false'
 
-      # Add parameters to only list for current monetary_account_id
-      params['monetary_account_id'] = monetary_account_id
-      params['display_user_event'] = 'false'
+        # Get events
+        events = endpoint.Event.list(
+            params=params,
+        ).value
 
-      # Get events
-      events = endpoint.Event.list(
-          params=params,
-      ).value
+        for event in events:
+            if event.object_.Payment:
+                payment = event.object_.Payment
+                # Check if it is a sent payment (afschrijving), amount must be negative
+                # TODO: Include requests (Ann requests groceries)
+                if payment.sub_type == "PAYMENT" and float(payment.amount.value) < 0:
+                    # Check if the payment has been split
+                    if payment.request_reference_split_the_bill:
+                        if is_buaut_split_the_bill(payment):
+                            # Break loop since this is where we left the last time
+                            break
+                        else:
+                            # Skip payment has been made manually
+                            continue
 
-      for event in events:
-        if event.object_.Payment:
-          payment = event.object_.Payment
-          if payment.request_reference_split_the_bill \
-                  and is_included(payment)
-                  and is_buaut_split_the_bill:
-            # Break loop since this is where we left the last time
-            break
+                    # Append payment to list to return
+                    unsplit_events.append(event)
 
-      # Return payment in need of splitting
-      return unsplit_payments
+        # Return payment in need of splitting
+        return unsplit_events
 
 
-def is_included(payment: Payment) -> bool:
-  """Find out if a payment is included
+def filter_excluded_events(events: List[endpoint.Event], includes: List[str], excludes: List[str]) -> List[endpoint.Event]:
+    """Filter all excluded payments
 
-  Args:
-      payment (Payment): Bunq payment object to validate
+    Args:
+        payments (Payment): Bunq payment object to validate
 
-  Returns:
-      bool: If payment is included in this batch
-  """
-  counterparty = payment.counterparty_alias.label_monetary_account
+    Returns:
+        List[endpint.Payment]: List of included payments
+    """
+    if not includes and not excludes:
+        # No need to check just return
+        return events
 
-  # When includes is defined check on includes to, else just check excludes
-  if INCLUDES:
-    return counterparty.iban in INCLUDES and \
-        counterparty.iban not in EXCLUDES
-  else:
-    return counterparty.iban not in EXCLUDES
+    included_events: List[endpoint.Event] = []
+    # Loop payments to filter
+    for event in events:
+        payment = event.object_.Payment
+        counterparty = payment.counterparty_alias.label_monetary_account
+
+        # When payment in excludes it should be included
+        if counterparty.iban not in excludes:
+            # When includes defined check if included, else just append
+            if includes:
+                if counterparty.iban in includes:
+                    included_events.append(payment)
+            else:
+                included_events.append(payment)
+
+    return included_events
+
+
+def is_buaut_split_the_bill(payment: endpoint.Payment) -> bool:
+    payment.request_reference_split_the_bill
+    return False
