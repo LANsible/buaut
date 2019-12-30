02c859a1ee78618b7f375025f078b5ee55ed28e9
wilmardo <info@wilmardenouden.nl>
2019-12-30 13:06:21 +0100

    feat: completed split, needs more testing
    Signed-off-by: wilmardo <info@wilmardenouden.nl>

--- buaut/command/split.py
+++ buaut/command/split.py
@@ -3,10 +3,10 @@ from typing import List, Tuple
 
 import click
 import validators
+import datetime
 from bunq.sdk.client import Pagination
 from bunq.sdk.exception import BunqException
 from bunq.sdk.model.generated import endpoint, object_
-from pprint import pprint
 
 from buaut import helpers
 
@@ -18,7 +18,7 @@ from buaut import helpers
     help='Email and percentage/amount to request',
     required=True,
     multiple=True,
-    type=(str, str)
+    type=(click.STRING, click.STRING)
 )
 @click.option(
     '--includes',
@@ -30,14 +30,20 @@ from buaut import helpers
     help='IBAN numbers to exclude',
     type=click.STRING
 )
-def split(ctx, get: List[Tuple[str, float]], includes: str, excludes: str):
+@click.option(
+    '--start',
+    help='Date from when should be split',
+    # formats are                1970-02-01, 01-02-1970, 02-01-1970, 70-02-01,   01-02-70,   02-01-70
+    type=click.DateTime(formats=['%Y-%m-%d', '%d-%m-%Y', '%m-%d-%Y', '%y-%m-%d', '%d-%m-%y', '%m-%d-%y'])
+)
+def split(ctx, get: List[Tuple[click.STRING, click.STRING]], includes: click.STRING, excludes: click.STRING, start: datetime):
     """Split payments to certain users
 
     Args:
         ctx ([type]): Click object containing the arguments from global
         get ([tuple]): List of users to request from
-        includes (str): Comma seperated string containing includes
-        excludes (str): Comma seperated string containing excludes
+        includes (click.STRING): Comma seperated string containing includes
+        excludes (click.STRING): Comma seperated string containing excludes
     """
     monetary_account_id: int = ctx.obj.get('monetary_account_id')
     currency: str = ctx.obj.get('currency')
@@ -54,16 +60,15 @@ def split(ctx, get: List[Tuple[str, float]], includes: str, excludes: str):
     # NOTE: Using events since we need to pass event_id to the requestInquiry
     # to get a correct reference
     unsplit_events: List[endpoint.Event] = get_all_unsplit_events(
-        monetary_account_id)
+        monetary_account_id=monetary_account_id,
+        includes=includes_list,
+        excludes=excludes_list,
+        end_date=start)
 
-    # Filter out all non relevant events
-    included_events: List[endpoint.Event] = filter_excluded_events(
-        events=unsplit_events, includes=includes_list, excludes=excludes_list)
-
-    if included_events:
+    if unsplit_events:
         # Create new list of tuples to fill with amounts
         requests: List[Tuple[str, float]] = []
-        for event in included_events:
+        for event in unsplit_events:
             # Split the bill!
             payment = get_payment_object(event.object_.Payment)
 
@@ -76,11 +81,16 @@ def split(ctx, get: List[Tuple[str, float]], includes: str, excludes: str):
                 'created': payment.created
             }
 
-            for email, percentage in get:
-                normalized_percentage: float = float(percentage) / 100
-                # Calculate amount to request
-                amount: float = amount_to_split * normalized_percentage
-                requests.append((email, amount))
+            # Create requests based on percentage or amount
+            for e, a in get:
+                if '%' in a:
+                    normalized_percentage: float = float(a) / 100
+                    # Calculate amount to request
+                    amount: float = amount_to_split * normalized_percentage
+                else:
+                    amount: float = float(a)
+
+                requests.append((e, amount))
 
             # Create request batch for payment
             helpers.create_request_batch(
@@ -96,7 +106,9 @@ def split(ctx, get: List[Tuple[str, float]], includes: str, excludes: str):
         exit(0)
 
 
-def get_all_unsplit_events(monetary_account_id: int) -> List[endpoint.Event]:
+def get_all_unsplit_events(
+        monetary_account_id: int, includes: List[str], excludes: List[str], end_date: datetime.datetime
+    ) -> List[endpoint.Event]:
     """Get all unsplit payments for a certain account
 
     Args:
@@ -113,7 +125,7 @@ def get_all_unsplit_events(monetary_account_id: int) -> List[endpoint.Event]:
     while True:
         # Check if first iteration (unsplit_payments empty)
         if not events:
-            # We will loop over the payments in baches of 50
+            # We will loop over the payments in batches of 50
             pagination = Pagination()
             pagination.count = 50
             params = pagination.url_params_count_only
@@ -133,24 +145,26 @@ def get_all_unsplit_events(monetary_account_id: int) -> List[endpoint.Event]:
             params=params,
         ).value
 
-        for event in events:
-            if event.object_.Payment:
-                payment = get_payment_object(event.object_.Payment)
+        # Filter out all non relevant events
+        included_events: List[endpoint.Event] = filter_excluded_events(
+            events=events, includes=includes, excludes=excludes)
+
+        for e in included_events:
+            if datetime.datetime.strptime(e.created, '%Y-%m-%d %H:%M:%S.%f') < end_date:
+                # Break the loop since this is before the end_date
+                break
+
+            if e.object_.Payment:
+                payment = get_payment_object(e.object_.Payment)
+
                 # Check if it is a sent payment (afschrijving), amount must be negative
                 if payment.sub_type == "PAYMENT" and float(payment.amount.value) < 0:
                     # Check if the payment has been split
                     if payment.request_reference_split_the_bill:
-                        if is_buaut_split_the_bill(payment):
-                            # Break loop since this is where we left the last time
-                            break
-                        # TODO: Add starting date option
-                        # elif payment.created
-                        else:
-                            # Skip request has been made manually
-                            continue
-
-                    # Append payment to list to return
-                    unsplit_events.append(event)
+                        continue
+                    else:
+                        # Append payment to list to return
+                        unsplit_events.append(e)
 
         # Return payment in need of splitting
         return unsplit_events
@@ -163,7 +177,7 @@ def filter_excluded_events(events: List[endpoint.Event], includes: List[str], ex
         payments (Payment): Bunq payment object to validate
 
     Returns:
-        List[endpint.Payment]: List of included payments
+        List[endpoint.Payment]: List of included payments
     """
     if not includes and not excludes:
         # No need to check just return
@@ -171,8 +185,8 @@ def filter_excluded_events(events: List[endpoint.Event], includes: List[str], ex
 
     included_events: List[endpoint.Event] = []
     # Loop payments to filter
-    for event in events:
-        payment = get_payment_object(event.object_.Payment)
+    for e in events:
+        payment = get_payment_object(e.object_.Payment)
         counterparty = payment.counterparty_alias.label_monetary_account
 
         # When payment not in excludes it should be included
@@ -186,15 +200,12 @@ def filter_excluded_events(events: List[endpoint.Event], includes: List[str], ex
 
     return included_events
 
-def is_buaut_split_the_bill(payment: endpoint.Payment) -> bool:
-    return False
-
 def get_payment_object(event: endpoint.Payment) -> endpoint.Payment:
     """Workaround for the issue https://github.com/bunq/sdk_python/issues/116
-    
+
     Args:
         event (endpoint.Payment): Payment object of Event object so incomplete
-    
+
     Returns:
         endpoint.Payment: Payment object but from the payment endpoint
     """
